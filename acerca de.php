d1d8dcca8131d43f8440ce0d4c9d5538aca014a1
DESKTOP-89DTGNK\Ricardo Gonzales <jose_gonzalezro@fet.edu.co>
2018-11-05 17:22:24 -0500

    version 1

---
+++ acerca de.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="jquery.mobile/jquery.mobile-1.4.5.min.css">
+    <link rel="stylesheet" href="jquery.mobile/jquery.mobile.icons-1.4.5.min.css">
+    <link rel="stylesheet" href="jquery.mobile/jquery.mobile.structure-1.4.5.min.css">
+    <link rel="stylesheet" href="themes/trabajo.min.css">
+    <script src="jquery.mobile/demos/js/jquery.min.js"></script>
+    <script src="jquery.mobile/demos/js/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+<div data-role="page" data-theme="c">
+        <div data-role="header">
+            <a href="index1.php" data-role="button" data-icon="home">Inicio</a>
+            <h1><img src="images/captura.png" alt=""></h1>
+            <h2> SERVICIOS A TU HABITACION</H2>
+
+        </div>
+<ul data-role="listview" data-inset="true" data-split-icon="gear" data-split-theme="d">
+                    <li data-role="list-divider">Escoge tu Habitacion</li>
+                    <li>
+                      <a href="#">
+                         <img src="images/xe.jpg" style="height:40px; width: 40%">
+                         <h2>Habitacion 1</h2>
+                         <p>Descripcion: cuenta con una cama,aire acondicionado,televisir, $100.000</p>
+                     </a>
+                    <a href="#promo" data-rel="popup" data-position-to="window" data-transition="pop">mas informacion </a>
+
+                    </li>
+                    <br>
+                    <br>
+
+                    <li data-role="list-divider">habitacion Personal</li>
+                    <li>
+                        <a href="#">
+                             <img src="images/xi.jpg" style="height:40px; width: 40%">
+                             <h2>promociones</h2>
+                             <p>Descripcion </p>
+                        </a>
+                        <a href="#promo" data-rel="popup" data-position-to="window" data-transition="pop">mas informacion </a>
+                    </li>
+                    <br>
+                    <br>
+
+                <ul data-role="listview" data-inset="true" data-split-icon="gear" data-split-theme="d">
+                    <li data-role="list-divider">Promociones especiales del dia</li>
+                    <li>
+                      <a href="#">
+                         <img src="images/xo.jpg" style="height:40px; width: 40%">
+                         <h2>promocion 3</h2>
+                         <p>descripcion de la promocion</p>
+                     </a>
+                    <a href="#promo" data-rel="popup" data-position-to="window" data-transition="pop">mas informacion </a>
+
+                    </li>
+                    <br>
+                    <br>
+                    <li data-role="list-divider">Promociones especiales del dia</li>
+                    <li>
+                      <a href="#">
+                         <img src="images/xu.jpg" style="height:40px; width: 40%">
+                         <h2>promocion 1</h2>
+                         <p>descripcion de la promocion</p>
+                     </a>
+                    <a href="#promo" data-rel="popup" data-position-to="window" data-transition="pop">mas informacion </a>
+
+                    </li>
+                    <br>
+                    <br>
+
+                    <div data-role="footer">
+            <img src="jquery.mobile/images/it.jpg" style="height:100px; width: 15%">
+            <img src="jquery.mobile/images/fb.png" style="height:100px; width: 15%">
+            <img src="jquery.mobile/images/te.png" style="height:100px; width: 15%">
+           <h4>maria camila nuñez molano     -tel:3133582617</h4>
+        </div>
+
+            </div>
---
+++ index1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="jquery.mobile/jquery.mobile-1.4.5.min.css">
+    <link rel="stylesheet" href="jquery.mobile/jquery.mobile.icons-1.4.5.min.css">
+    <link rel="stylesheet" href="jquery.mobile/jquery.mobile.structure-1.4.5.min.css">
+    <link rel="stylesheet" href="jquery.mobile/themes/maria.min.css">
+    <script src="jquery.mobile/demos/js/jquery.min.js"></script>
+    <script src="jquery.mobile/demos/js/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+    <div data-role="page" data-theme="c">
+        <div data-role="header">
+            <a href="index.php" data-role="button" data-icon="home">Inicio</a>
+            <h1><img src="images/Captura.gif" alt=""></h1>
+           
+        </div>
+        <div data-role="main">
+            <div data-role="navbar">
+                <ul>
+                    <li><a data-icon="grid" href="acerca de.php">Acerca de</a></li>
+                    <li><a data-icon="star" href="HOTEL.php">HOTEL</a></li>
+                    <li><a data-icon="star" href="iniciar-sesion.php">Iniciar Sesion</a></li>
+
+                </ul>
+            </div>
+
+            <div data-role="content">
+		        <img src="images/xa.jpg" style="height:600px; width: 100%"> 
+                
+
+            </div>
+            <br> <br> <br>
+              
+           
+
+      <div data-role="footer">
+            <img src="jquery.mobile/images/it.jpg" style="height:100px; width: 15%"> 
+            <img src="jquery.mobile/images/fb.png" style="height:100px; width: 15%"> 
+            <img src="jquery.mobile/images/te.png" style="height:100px; width: 15%"> 
+           <h4>maria camila nuñez molano     -tel:3133582617</h4>
+        </div>
+    </div>
+</body>
+</html>          
